0bdb7b253e3a37d4435525394c0fbfc52575e206
Luth-Badila <FrancescoLutfi10@gmail.com>
2023-08-24 22:12:46 +0700

    unfinished slider image

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
-const slides = document.querySelectorAll(".slide");
-let currentSlide = 0;
+// In HTML, .display-area has the width of 4 cards = 880px. Each card is 200px width and margin set to 10px.
+// .display-area has a .cards-wrapper which contains all the cards. .cards-wrapper is set to display flex.
+// .display-area has overflow hidden to hide the portion of cards-wrapper which extends beyond the container's width.
 
-function showSlide(index) {
-  slides.forEach((slide) => slide.classList.remove("active"));
-  slides[index].classList.add("active");
-}
+const wrapper = document.querySelector(".cards-wrapper");
+// console.log(wrapper.clientWidth);
 
-function nextSlide() {
-  currentSlide = (currentSlide + 1) % slides.length;
-  showSlide(currentSlide);
-}
+// grab the dots
+const dots = document.querySelectorAll(".dot");
+// the default active dot num which is array[0]
+let activeDotNum = 0;
 
-function prevSlide() {
-  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-  showSlide(currentSlide);
-}
+dots.forEach((dot, idx) => {
+  //   number each dot according to array index
+  dot.setAttribute("data-num", idx);
 
-// Show the first slide initially
-showSlide(currentSlide);
-
-// Auto switch slides every 3 seconds
-setInterval(nextSlide, 3000);
+  //   add a click event listener to each dot
+  dot.addEventListener("click", (e) => {
+    let clickedDotNum = e.target.dataset.num;
+    // console.log(clickedDotNum);
+    //     if the dot clicked is already active, then do nothing
+    if (clickedDotNum == activeDotNum) {
+      // console.log('active');
+      return;
+    } else {
+      // console.log('not active');
+      // shift the wrapper
+      let displayArea = wrapper.parentElement.clientWidth;
+      console.log(displayArea);
+      // let pixels = -wrapper.clientWidth * clickedDotNum;
+      let pixels = -displayArea * clickedDotNum;
+      wrapper.style.transform = "translateX(" + pixels + "px)";
+      //       remove the active class from the active dot
+      dots[activeDotNum].classList.remove("active");
+      //       add the active class to the clicked dot
+      dots[clickedDotNum].classList.add("active");
+      //       now set the active dot number to the clicked dot;
+      activeDotNum = clickedDotNum;
+    }
+  });
+});
