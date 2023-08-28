40028d55c5b6b5e20ff7df62f5fabdfbd82e7238
Luth-Badila <FrancescoLutfi10@gmail.com>
2023-08-28 19:45:12 +0700

    added lazy loading

--- script.js
+++ script.js
@@ -1,41 +1,27 @@
-// In HTML, .display-area has the width of 4 cards = 880px. Each card is 200px width and margin set to 10px.
-// .display-area has a .cards-wrapper which contains all the cards. .cards-wrapper is set to display flex.
-// .display-area has overflow hidden to hide the portion of cards-wrapper which extends beyond the container's width.
+document.addEventListener("DOMContentLoaded", function () {
+  const imagesToLoad = document.querySelectorAll("img[data-src]");
 
-const wrapper = document.querySelector(".cards-wrapper");
-// console.log(wrapper.clientWidth);
+  const options = {
+    threshold: 0.5, // Intersection ratio at which the image will be loaded
+  };
 
-// grab the dots
-const dots = document.querySelectorAll(".dot");
-// the default active dot num which is array[0]
-let activeDotNum = 0;
+  const loadImage = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+      image.removeAttribute("data-src");
+    };
+  };
 
-dots.forEach((dot, idx) => {
-  //   number each dot according to array index
-  dot.setAttribute("data-num", idx);
+  const imageObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        loadImage(entry.target);
+        observer.unobserve(entry.target);
+      }
+    });
+  }, options);
 
-  //   add a click event listener to each dot
-  dot.addEventListener("click", (e) => {
-    let clickedDotNum = e.target.dataset.num;
-    // console.log(clickedDotNum);
-    //     if the dot clicked is already active, then do nothing
-    if (clickedDotNum == activeDotNum) {
-      // console.log('active');
-      return;
-    } else {
-      // console.log('not active');
-      // shift the wrapper
-      let displayArea = wrapper.parentElement.clientWidth;
-      console.log(displayArea);
-      // let pixels = -wrapper.clientWidth * clickedDotNum;
-      let pixels = -displayArea * clickedDotNum;
-      wrapper.style.transform = "translateX(" + pixels + "px)";
-      //       remove the active class from the active dot
-      dots[activeDotNum].classList.remove("active");
-      //       add the active class to the clicked dot
-      dots[clickedDotNum].classList.add("active");
-      //       now set the active dot number to the clicked dot;
-      activeDotNum = clickedDotNum;
-    }
+  imagesToLoad.forEach((image) => {
+    imageObserver.observe(image);
   });
 });
